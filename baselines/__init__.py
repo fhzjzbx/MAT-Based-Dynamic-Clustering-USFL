8640a84f2a97d1d1b9d4abc6c8c9cb2c109d3941
风花直角坐标系\fhzjz <1989764384@qq.com>
2026-09-06 10:44:21 +0800

    paperC提交内容

--- baselines/__init__.py
+++ baselines/__init__.py
@@ -1,5 +1,6 @@
-from cpsl.cpsl_agent import CPSLAgent
-from clustersfl.clustersfl_agent import ClusterSFLAgent
-from pcsfl.pcsfl_agent import PCSFLAgent
+# 使用相对导入；仓库原文件使用绝对导入，容易在 `from baselines import ...` 时失败。
+from .cpsl.cpsl_agent import CPSLAgent
+from .clustersfl.clustersfl_agent import ClusterSFLAgent
+from .pcsfl.pcsfl_agent import PCSFLAgent
 
 __all__ = ["CPSLAgent", "ClusterSFLAgent", "PCSFLAgent"]
